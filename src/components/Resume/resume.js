59108a9161500dceb4ed5e46b61014e8974289a6
SMIT PANCHAL <panc7220@mylaurier.ca>
2024-01-15 18:25:32 -0500

    added download resume button

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -4,10 +4,13 @@ const Resume = () => {
 	return (
 		<section id='resume'>
 			<h2 className='resume-title'>Resume</h2>
-			<span className='resume-desc'>Presented below is my resume, offering a concise yet comprehensive overview of my skills and experiences. Feel free to explore for more details about my professional background.</span>
-
+			<span className='resume-desc'>
+			Discover my concise and comprehensive resume for insights into my skills, experiences and achievements. Download the PDF by clicking the below button for convenient access.
+			</span>
+			<a href={require('./resume.pdf')} className='download-btn' download='Smit-Panchal-Resume.pdf'>Download Resume</a>
 			<div className='resume-pdf'>
 				<iframe src={require('./resume.pdf')} title='PDF viewer' allowfullscreen></iframe>
+				 
 			</div>
 		</section>
 	);
